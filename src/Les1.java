1f494a91c0f255a5bfa023360448d727d7e16b64
neqelovek <neqelovek@gmail.com>
2022-12-18 22:29:42 +0300

    task1 finish

--- src/Les1.java
+++ src/Les1.java
@@ -1,4 +1,16 @@
 public class Les1 {
     public static void main(String[] args) {
+
+        sumTwoNumbers(1,2);
+        sumTwoNumbers(11,2);
+        sumTwoNumbers(11,22);
+    }
+
+
+    //  Task 1: Написать метод, принимающий на вход два целых числа и проверяющий,
+    //  что их сумма лежит в пределах от 10 до 20 (включительно),
+    //  если да – вернуть true, в противном случае – false.
+    private static void sumTwoNumbers (int a, int b){
+        System.out.println(10 <= (a+b) && (a+b) <= 20 );
     }
 }
