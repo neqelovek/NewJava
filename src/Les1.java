4fb6714433d55ecfcc0993a7d2789b8e9952d5a2
neqelovek <neqelovek@gmail.com>
2022-12-18 23:46:21 +0300

    task3 and 4 finish

--- src/Les1.java
+++ src/Les1.java
@@ -6,10 +6,18 @@ public class Les1 {
         sumTwoNumbers(1,2);
         sumTwoNumbers(11,2);
         sumTwoNumbers(11,22);
+        System.out.println();
 
-        rationalNumbers();
+        rationalNumber();
+        System.out.println();
+
+        rationalNum(12);
+        rationalNum(-13);
+        System.out.println();
+
+        numbersLine();
+
     }
-
 
     //  Task 1: Написать метод, принимающий на вход два целых числа и проверяющий,
     //  что их сумма лежит в пределах от 10 до 20 (включительно),
@@ -18,10 +26,11 @@ public class Les1 {
         System.out.println(10 <= (a+b) && (a+b) <= 20 );
     }
 
-    //    Task 2:Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    //    Task 2:Написать метод, которому в качестве параметра передается целое число,
+    //    метод должен напечатать в консоль,
     //    положительное ли число передали или отрицательное.
     //    Замечание: ноль считаем положительным числом.
-    private static void rationalNumbers(){
+    private static void rationalNumber(){
         Scanner input = new Scanner(System.in);
         System.out.println("Введите рациональное число");
         int num = input.nextInt();
@@ -29,5 +38,27 @@ public class Les1 {
             System.out.println("Ваше число " + num + " положительное!");
         }else
             System.out.println("Ваше число " + num + " отрицательное!");
+    }
+
+    //    Task 3:Написать метод, которому в качестве параметра передается целое число.
+    //    Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+
+    private static void rationalNum(int a){
+        System.out.println(0 >= a);
+    }
+
+    //    Task 4:Написать метод, которому в качестве аргументов передается строка и число,
+    //    метод должен отпечатать в консоль указанную строку, указанное количество раз;
+
+    private static void numbersLine(){
+        Scanner lines = new Scanner(System.in);
+        System.out.println("Введите строку ");
+        String string = lines.nextLine();
+        Scanner num = new Scanner(System.in);
+        System.out.println("Введите количество нужных строк ");
+        int number = num.nextInt();
+        for (int i = 0; i < number; i++) {
+            System.out.println(string);
+        }
     }
 }
