aa6abdb52602d8551a6796afdbae18e14c319ffb
neqelovek <neqelovek@gmail.com>
2022-12-19 00:28:46 +0300

    task 6 finish

--- src/Les1.java
+++ src/Les1.java
@@ -20,6 +20,9 @@ public class Les1 {
 
         leapYear(2016);
         leapYear(2022);
+        System.out.println();
+
+        arrays();
 
     }
 
@@ -72,5 +75,18 @@ public class Les1 {
 
     private static void leapYear(int a){
         System.out.println(a % 4 == 0 && a % 100 != 0 || a % 400 == 0);
+    }
+
+    //    Task 6:Задать целочисленный массив, состоящий из элементов 0 и 1.
+    //    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+    private static void arrays() {
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                array[i] = 0;
+            } else array[i] = 1;
+            System.out.print(array[i] + " ");
+        }
     }
 }
