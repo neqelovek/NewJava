19d32908895c2ea3d24c6b3bb68fc6636f5e4286
neqelovek <neqelovek@gmail.com>
2022-12-18 23:04:36 +0300

    task2 finish

--- src/Les1.java
+++ src/Les1.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Les1 {
     public static void main(String[] args) {
 
         sumTwoNumbers(1,2);
         sumTwoNumbers(11,2);
         sumTwoNumbers(11,22);
+
+        rationalNumbers();
     }
 
 
@@ -12,5 +16,18 @@ public class Les1 {
     //  если да – вернуть true, в противном случае – false.
     private static void sumTwoNumbers (int a, int b){
         System.out.println(10 <= (a+b) && (a+b) <= 20 );
+    }
+
+    //    Task 2:Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    //    положительное ли число передали или отрицательное.
+    //    Замечание: ноль считаем положительным числом.
+    private static void rationalNumbers(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите рациональное число");
+        int num = input.nextInt();
+        if (num >= 0){
+            System.out.println("Ваше число " + num + " положительное!");
+        }else
+            System.out.println("Ваше число " + num + " отрицательное!");
     }
 }
