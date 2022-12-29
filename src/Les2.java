2abf60a476fa187f9b23f4590812a20bb3246f95
neqelovek <neqelovek@gmail.com>
2022-12-29 20:00:08 +0300

    Task 1 finish

--- src/Les2.java
+++ src/Les2.java
@@ -1,2 +1,24 @@
 public class Les2 {
+    public static void main(String[] args) {
+
+        isPalindrome("шалаш");
+        System.out.println();
+        isPalindrome("abcdedcba");
+        System.out.println();
+        isPalindrome("GeekBrains");
+
+    }
+
+    //    Task 1: Напишите метод, который принимает на вход строку (String) и
+    //    определяет является ли строка палиндромом (возвращает boolean значение).
+    //    шалаш
+    //    abcdedcba
+
+    public static void isPalindrome(String string) {
+        StringBuilder reversStr = new StringBuilder();
+        for (int i = string.length() - 1; i >= 0; --i)
+            reversStr.append(string.charAt(i));
+        System.out.println(string.equals(reversStr.toString()));
+    }
 }
+
