69e2d876287ff5579e337652fe0380e0963f7a4b
neqelovek <neqelovek@gmail.com>
2023-01-02 11:36:02 +0300

    Task 1 finish

--- src/Les3.java
+++ src/Les3.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Les3 {
     public static void main(String[] args) {
 
+        oddNumbers();
+
+    }
+
+    //     Task 1. Пусть дан произвольный список целых чисел,
+    //    удалить из него четные числа (в языке уже есть что-то готовое для этого)
+
+    public static void oddNumbers(){
+        int [] numb = {1,2,3,4,5,6,7,8,9};
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int j : numb) numbers.add(j);
+
+        ArrayList<Integer> odd = new ArrayList<>();
+        for (Integer n : numbers) {
+            if (n % 2 != 0)
+                odd.add(n);
+        }
+        System.out.println(odd);
     }
 }
