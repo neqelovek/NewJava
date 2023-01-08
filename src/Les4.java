94c3911a723210ce66f61f0380cbfe15835c72a8
neqelovek <neqelovek@gmail.com>
2023-01-08 19:13:41 +0300

    Lesson 4 start

--- src/Les4.java
+++ src/Les4.java
@@ -1,2 +1,22 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Les4 {
+    public static void main(String[] args) {
+
+    }
+
+    //    1. Реализовать консольное приложение, которое:
+    //  Принимает от пользователя строку вида
+    //  text~num
+    //  Нужно рассплитить строку по ~, сохранить text в связный список на позицию num.
+    //  Если введено print~num, выводит строку из позиции num в связном списке и удаляет её из списка.
+    //  > first~1
+    //  > third~3
+    //  > print~1
+    //  < first
+    //  > print~2
+    //  < throw new IllegalArgumentException(); (напечатать, что такого элемента нет)
 }
+
